c8ae8ff349964b280a0d7445a1b3f9558e503271
Kain <drkain@outlook.com>
2021-03-18 16:23:08 +1300

    General improvements to code

--- main.js
+++ main.js
@@ -1,83 +1,114 @@
-// Pour installer les dependances / To install dependencies :
-// Dans la console tapez: / In console type:
-// npm init
-// npm i word-wrap && npm i node-fetch && npm i fs && npm i xml-js && npm i canvas
-
-// SCRIPT BY DALVI (https://github.com/Dalvii/)
-
+// https://github.com/Dalvii/plex-library-export
 const wrap = require('word-wrap');
 const fetch = require('node-fetch');
 const fs = require('fs');
 const convert = require('xml-js');
 const Canvas = require('canvas');
-const { registerFont, createCanvas } = require('canvas')
-registerFont('bahnschrift.ttf', { family: 'Bahnschrift' })
+const { registerFont } = require('canvas');
+
+registerFont('assets/bahnschrift.ttf', { family: 'Bahnschrift' });
 
 // GET PLEX TOKEN: https://support.plex.tv/articles/204059436-finding-an-authentication-token-x-plex-token/
+const PLEX_TOKEN = '';
+const PLEX_URL = 'http://localhost:32400';
+const SECTION = '0';
+const MODE = 'unique'; // 'unique' or 'mutiple' for 1 single image or several for each page 
 
-const PLEX_API = '?X-Plex-Token=YOUR_PLEX_TOKEN'
-const PLEX_URL = 'http://your_server:32400'
-const SECTION = '1'
-const MODE = 'mutiple' // 'unique' ou 'mutiple' pour 1 seul image ou divisé plusieurs
+const getURL = (type) => {
+    switch (type) {
+        case 'all':
+            return `${PLEX_URL}/library/sections/${SECTION}/all?X-Plex-Token=${PLEX_TOKEN}`;
+        case 'sections':
+            return `${PLEX_URL}/library/sections/all?X-Plex-Token=${PLEX_TOKEN}`;
+    }
+}
+
+const truncate = (str, n) => (str.length > n) ? str.substr(0, n - 1) + '...' : str;
 
 async function get() {
-    if (Number(SECTION) >= 0) {} else return console.log(`Search 'key=' corresponding to your Library and put this number in 'SECTION =' ${PLEX_URL}/library/sections/all?X-Plex-Token=${PLEX_API}`),
-    console.log(`\nCherchez 'key=' dans le lien suivant et mettez dans 'SECTION =' le numero correspondant a votre bibliotheque ${PLEX_URL}/library/sections/all?X-Plex-Token=${PLEX_API}`)
+    // Ensure MODE is valid
+    if (!['unique', 'multiple'].includes(MODE)) {
+        console.log('[Error] MODE must be either \'unique\' or \'multiple\'');
+        process.exit(0);
+    }
 
-    // get plex xml api result : xml -> json
-    const xml = await fetch(`${PLEX_URL}/library/sections/${SECTION}/all${PLEX_API}`, {method: 'GET'}).then(response => response.text()).catch(console.error);
-    let json = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4})).MediaContainer.Directory == undefined ? JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4})).MediaContainer.Video : JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4})).MediaContainer.Directory
+    // Ensure PLEX_TOKEN is set
+    if (PLEX_TOKEN === '') {
+        console.log('[Error] PLEX_TOKEN is not set on line 11');
+        process.exit(0);
+    }
 
-    const poster_list = []
-    const title_list = []
-    json.forEach(i => {
-        poster_list.push(PLEX_URL + i._attributes.thumb + PLEX_API)
-        title_list.push(i._attributes.title.length > 31 ? i._attributes.title.substring(-i._attributes.title.length,31)+'...' : i._attributes.title)
+    // If a library key is not set, print out the available keys
+    if (Number(SECTION) === 0) {
+        const xml = await fetch(getURL('sections'), { method: 'GET' }).then(res => res.text()).catch(console.error);
+        const json = JSON.parse(convert.xml2json(xml, { compact: true, spaces: 4 }));
+        console.log('[Error] SECTION needs to be assigned a library key on line 14');
+        json.MediaContainer.Directory.map(item => {
+            const library = item._attributes;
+            console.log(`${library.key} = ${library.title} (${library.type})`)
+        });
+        process.exit(0);
+    }
+
+    // Fetch the contents of the library and convert XML to JSON
+    const xml = await fetch(getURL('main'), { method: 'GET' }).then(response => response.text()).catch(console.error);
+    const json = JSON.parse(convert.xml2json(xml, { compact: true, spaces: 4 }));
+    const content = json.MediaContainer.Directory == undefined ? json.MediaContainer.Video : json.MediaContainer.Directory;
+    const library = json.MediaContainer._attributes.librarySectionTitle;
+
+    const poster_list = [];
+    const title_list = [];
+
+    content.forEach(i => {
+        poster_list.push(`${PLEX_URL}${i._attributes.thumb}?X-Plex-Token=${PLEX_TOKEN}`);
+        title_list.push(truncate(i._attributes.title, 31));
     });
 
-    const library = JSON.parse(convert.xml2json(xml, {compact: true, spaces: 4})).MediaContainer._attributes.librarySectionTitle
     console.log(library)
     console.log('Script by Dalvi https://github.com/Dalvii/')
-    console.log('Il y a '+title_list.length+ ' éléments en tout')
+    console.log('There are ' + title_list.length + ' elements total')
 
-    let y_total = Math.ceil(poster_list.length/7) * 430 + 300
-    let x_pos = [150, 428.3, 706.6, 984.9, 1263.2, 1541.5, 2200-150-230]
+    let y_total = Math.ceil(poster_list.length / 7) * 430 + 300
+    let x_pos = [150, 428.3, 706.6, 984.9, 1263.2, 1541.5, 2200 - 150 - 230]
     let y_pos = -280
 
-    let pages = (MODE === 'unique' ? 1 : Math.ceil(poster_list.length/7/7))
+    let pages = (MODE === 'unique' ? 1 : Math.ceil(poster_list.length / 7 / 7))
 
     for (let y = 0; y < pages; y++) {
-        
-        console.log('page '+(y+1)+' avec '+(MODE === 'unique' ? poster_list.length : Math.min(poster_list.length, 49))+' images...')
+
+        console.log('page ' + (y + 1) + ' with ' + (MODE === 'unique' ? poster_list.length : Math.min(poster_list.length, 49)) + ' images...')
 
         const canvas = Canvas.createCanvas(2200, (MODE === 'unique' ? y_total : 3310));
         const ctx = canvas.getContext('2d');
         ctx.font = 'bold 23px "Bahnschrift"';
         ctx.fillStyle = '#ffffff';
-    
+
         // background set
-        const bg = await Canvas.loadImage('./bg.png');
+        const bg = await Canvas.loadImage('./assets/background.png');
         ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
-        
+
         for (let i = 0; i < (MODE === 'unique' ? poster_list.length : Math.min(poster_list.length, 49)); i++) {
-            if (i%7 == 0) y_pos += 430;
+            if (i % 7 == 0) y_pos += 430;
             const poster = await Canvas.loadImage(poster_list[i]);
-            ctx.drawImage(poster, x_pos[i%7], y_pos, 230, 340);
-            ctx.fillText(wrap(title_list[i], {width: 21}), x_pos[i%7]-5, y_pos+368)
+            ctx.drawImage(poster, x_pos[i % 7], y_pos, 230, 340);
+            ctx.fillText(wrap(title_list[i], { width: 21 }), x_pos[i % 7] - 5, y_pos + 368);
         }
 
+        // Reset + Deletes the first 49 elements for multiple mode 
+        y_pos = -280;
+        poster_list.splice(0, 49);
+        title_list.splice(0, 49);
 
-        // Reset+supprime les 49 premiers elements pour le mode multiple
-        y_pos = -280
-        poster_list.splice(0,49)
-        title_list.splice(0,49)
-        
         // Build image
-        canvas.toBuffer()
-        const out = fs.createWriteStream(__dirname + '/'+library+'_'+y+'.jpg')
-        const stream = canvas.createJPEGStream()
-        stream.pipe(out)
-        out.on('finish', () => { console.log('Terminé !') })
+        canvas.toBuffer();
+        fs.mkdirSync(__dirname + '/output');
+
+        const out = fs.createWriteStream(__dirname + '/output/' + library + '_' + y + '.jpg');
+        const stream = canvas.createJPEGStream();
+
+        stream.pipe(out);
+        out.on('finish', () => { console.log('Completed!') });
     }
 }
+
 get()
